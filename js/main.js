0bfaa26204034a85377d3669c358de4f2d374f42
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-08-23 09:27:32 +0300

    disable double click (zoom) and context Menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+// disable double click (zoom) and context Menu
+$(function () {
+  document.addEventListener("dblclick", function (e) {
+    e.preventDefault();
+  });
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
+});
+
 import { Game } from "./game.js";
 let game = new Game();
